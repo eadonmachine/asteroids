1ebc9523b321c98373adab35cb68d26bc4679fd4
eadonmachine <eadonmachine@gmail.com>
2025-01-21 19:03:28 +0000

    add clock, use delta time

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
